92a0024949892605f7b6d5157db5fc8a0906e2ae
lisyaoran51 <lisyaoran51@hotmail.com>
2018-04-29 02:36:07 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <linux/module.h> 
+
+static unsigned int max = 5;
+module_param(max, uint, 0);
+MODULE_PARM_DESC(max,  "Maximum bytes per IO");
+
+static int __init hello_init(void){
+	printk("max=%d\n", max);
+	return 0;
+
+}
+
+static void __exit hello_exit(void){
+	prink("\n");
+
+}
